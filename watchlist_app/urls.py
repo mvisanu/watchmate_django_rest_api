cb73dbfdff994b6155356f9398be1dff9817c401
Visanu <mvisanu@gmail.com>
2022-04-01 15:14:03 -0500

    Add Class-based views

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,7 +1,11 @@
-# from django.urls import path, include
-# from watchlist_app.views import movie_list, movie_details
+from django.urls import path, include
+#from watchlist_app.views import movie_list, movie_details
+from watchlist_app.views import MovieListAV, MovieDetailAV
 
-# urlpatterns = [
-#    path('list/', movie_list, name='movie-list'),
-#    path('<int:pk>', movie_details, name='movie-detail'),
-# ]
+urlpatterns = [
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+   path('<int:pk>', MovieDetailAV.as_view(), name='movie-detail'),
+   #path('list/', movie_list, name='movie-list'),
+   #path('<int:pk>', movie_details, name='movie-detail'),
+   
+]
