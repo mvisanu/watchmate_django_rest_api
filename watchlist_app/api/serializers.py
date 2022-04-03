3443be20885c2cdf8f4c40bdbec9eb7bfaaaad74
Visanu <mvisanu@gmail.com>
2022-04-03 09:42:32 -0500

    Add user to review

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,14 +3,16 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):     
+    review_user = serializers.StringRelatedField(read_only=True)
+    
     class Meta:
         model = Review
         exclude = ('watchlist',)
         # fields = "__all__"
         
 class WatchListSerializer(serializers.ModelSerializer):   
-    #review = ReviewSerializer(many=True, read_only=True)
-    platform = serializers.CharField(source='platform.name')
+    reviews = ReviewSerializer(many=True, read_only=True)
+    #platform = serializers.CharField(source='platform.name')
     
     class Meta:
         model = WatchList
